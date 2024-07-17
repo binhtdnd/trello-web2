0a53b9493394516831d9e11b827d04174f2892aa
binhtd <binhtdnd@gmail.com>
2024-07-17 21:47:51 +0700

    add icon for light dark select mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
-import Button from '@mui/material/Button';
+import Button from '@mui/material/Button'
 
-import  Typography  from '@mui/material/Typography';
+import  Typography  from '@mui/material/Typography'
 
 import {
-  useColorScheme,
-} from '@mui/material/styles';
+  useColorScheme
+} from '@mui/material/styles'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Box from '@mui/material/Box';
+import LightModeIcon from '@mui/icons-material/LightMode'
+import  DarkModeOutLinedIcon  from '@mui/icons-material/DarkModeOutlined'
+import  SettingsBrightnessIcon  from '@mui/icons-material/SettingsBrightness'
 
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode);
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="label-select-dark-light-mode"
+        value={mode}
+        label="Age"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{display:'flex',alignItems:'center',gap:'8px'}}>
+          <LightModeIcon fontSize='small'/>  Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{display:'flex',alignItems:'center',gap:'8px'}}>
+            <DarkModeOutLinedIcon />  Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+        <div style={{display:'flex',alignItems:'center' ,gap:1}}>
+          <SettingsBrightnessIcon fontSize='small'/>  System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme();
@@ -23,6 +67,8 @@ function ModeToggle() {
 function App() {
   return (
     <>
+    <ModeSelect/>
+    <hr></hr>
     <ModeToggle/>
     <hr></hr>
     <div>BINH text containt outline</div>
