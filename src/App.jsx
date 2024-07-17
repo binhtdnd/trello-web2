aed3e84d191e2d260790d1fb5d0805941186dfdc
binhtd <binhtdnd@gmail.com>
2024-07-17 21:10:40 +0700

    update light mode/dark mode - video 12

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,29 @@ import Button from '@mui/material/Button';
 
 import  Typography  from '@mui/material/Typography';
 
+import {
+  useColorScheme,
+} from '@mui/material/styles';
+
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
+
 function App() {
   return (
     <>
+    <ModeToggle/>
+    <hr></hr>
     <div>BINH text containt outline</div>
       <Typography variant='body2' color="text.secondary">asssssssssaaa</Typography>
 
