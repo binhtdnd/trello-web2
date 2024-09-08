497aece713581869d291e4501c36c8464a766e7b
binhtdnd <binhtdnd@gmail.com>
2024-09-08 15:42:52 +0700

    deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,14 @@
 
-let apiRoot = ''
-if (process.env.BUILD_MODE === 'dev') {
-  apiRoot = 'http://localhost:8017'
-}
+// let apiRoot = ''
+// if (process.env.BUILD_MODE === 'dev') {
+//   apiRoot = 'http://localhost:8017'
+// }
 
-if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://trello-api-ft6z.onrender.com'
-}
+// if (process.env.BUILD_MODE === 'production') {
+//   apiRoot = 'https://trello-api-ft6z.onrender.com'
+// }
 
-export const API_ROOT = apiRoot
+export const API_ROOT = 'https://trello-api-ft6z.onrender.com'
 
 
 export const hardBoardId = '66d9cc270396bd4016348ad5'
