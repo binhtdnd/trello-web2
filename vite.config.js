6b452b0babc6af21badc550344517b7ed59289b7
binhtdnd <binhtdnd@gmail.com>
2024-09-08 15:45:43 +0700

    delete cross-env

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    'process.env': process.env
-  },
   plugins: [
     react(),
     svgr()
